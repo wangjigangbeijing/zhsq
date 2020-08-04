c24055eea3a3714933c45e1f8d961fc9c00b9fd8
hw65350007/hw65350007 <xiyoucjp0209@163.com>
2020-08-04 22:18:25 +0800

    修改bug

--- WebRoot/app/nfw/sqbsfwDetail.js
+++ WebRoot/app/nfw/sqbsfwDetail.js
@@ -11,6 +11,12 @@
 
 $(document).ready(function (){
 	
+	$("#characterQry").select2({	 
+		multiple: true
+	});
+	
+	$("#characterQry").val('abc').trigger("change"); //赋值一个不存在的value,解决默认选择第一个的问题
+	
 	if(curId != '')
 		viewDetail(curId);
 	
@@ -102,7 +108,7 @@ function loadsxxl(){
 			if(obj.success)
 			{
 				for(var i = 0; i < obj.list.length; i++){
-					var content = "<option value='" + obj.list[i].sxlb + "'>" + obj.list[i].sxlb + "</option>"
+					var content = "<option value='" + obj.list[i].sxmc + "'>" + obj.list[i].sxmc + "</option>"
 					$("#blsxxl").append(content);
 				}
 				
